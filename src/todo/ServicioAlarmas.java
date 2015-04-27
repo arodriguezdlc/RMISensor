3976056e082e2ba7217689474499067e2f595a85
salas <jmpa92@gmail.com>
2015-04-27 13:46:12 +0200

    Cambio final

--- src/todo/ServicioAlarmas.java
+++
@@ -1,8 +0,0 @@
-import java.rmi.*;
-import java.util.*;
-
-interface ServicioAlarmas extends Remote {
-	void alta(Sensor s) throws RemoteException;
-	void baja(Sensor s) throws RemoteException;
-	void enviaListaAlarmas(List<Alarma> listaAlarmasActivadas) throws RemoteException;
-}
